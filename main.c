7bffa4acb21d0da97f82d972e2cc8db58a79c214
AniKrisn <anirudh.krishnan@hotmail.com>
2024-10-08 05:38:23 +0530

    add fn

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
 struct list_item {
-    
+    int val;
+    struct list_item *next;
+};
+
+struct list {
+    struct list_item *head;
+};
+
+typedef struct list_item list_item;
+typedef struct list list;
+
+void remove_elegant(list *l, list_item *target) {
+    list_item **p = &l->head;
+    while (*p != target) {
+        p = &(*p)->next; 
+    }
+    p = target->next;
 }
